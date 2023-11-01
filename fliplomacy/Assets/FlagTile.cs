e6fe313241dd2e3bbfd912ebbf484b18164655bc
Viet <ntv2kpv@gmail.com>
2023-11-01 12:10:46 +0700

    update

--- fliplomacy/Assets/FlagTile.cs
+++ fliplomacy/Assets/FlagTile.cs
@@ -12,18 +12,22 @@ public class FlagTile : MonoBehaviour
     }
     public void ChangeFlag()
     {
+        //var flag = gameObject.transform.GetChild(1);
+        //if (flagStatus == 0)
+        //{
+        //    flagStatus = 1;
+        //    flag.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        //    flag.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        //}
+        //else
+        //{
+        //    flagStatus = 0;
+        //    flag.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        //    flag.gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        //}
         var flag = gameObject.transform.GetChild(1);
-        if (flagStatus == 0)
-        {
-            flagStatus = 1;
-            flag.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-            flag.gameObject.transform.GetChild(1).gameObject.SetActive(true);
-        }
-        else
-        {
-            flagStatus = 0;
-            flag.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-            flag.gameObject.transform.GetChild(1).gameObject.SetActive(false);
-        }
+        StartCoroutine(0.1f.Tweeng((p) => flag.gameObject.transform.GetChild(0).gameObject.transform.localEulerAngles = p,
+            flag.gameObject.transform.GetChild(0).gameObject.transform.localEulerAngles,
+            flag.gameObject.transform.GetChild(0).gameObject.transform.localEulerAngles + new Vector3(0, 180, 0)));
     }
 }
